bde317efb3bdfd2a8fb1dd37cdfa74bd85a8be8c
Etvaldo Lisboa <etvaldolisboa@hotmail.com>
2023-07-29 17:52:03 -0400

    add funcionalidades de tema

--- src/js/trocar-tema.js
+++ src/js/trocar-tema.js
@@ -1,11 +1,5 @@
-function changeTheme() {
-    const themeSwitch = document.querySelector('#theme-switch');
-    const theme = themeSwitch.checked ? 'dark' : 'light';
-  
-    // Set the theme
-    document.body.className = theme;
-  }
-  
-  // Add an event listener to the switch
-  themeSwitch.addEventListener('change', changeTheme);
-  
+const btnTrocaTema = document.getElementById('trocar-tema');
+
+btnTrocaTema.addEventListener('change', () => {
+  document.body.classList.toggle('modo-escuro')
+})
